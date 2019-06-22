1537903e7847f35e27d8c1a0127029087336b263
ccpcc1 <crisrxn12@gmail.com>
2019-06-22 17:58:13 -0500

    consulta Achivement lista, falta estilos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {HashRouter,Route,Switch,Redirect} from 'react-router-dom';
 import Menu from './components/Menu';
 import NotFound from './components/NotFound';
-import ListEmployees from './components/ListEmployees';
+import ListEmployees from './Containers/ListEmployees';
+import ListAchievement from './Containers/ListAchievements';
 import './App.css';
 
 
@@ -10,16 +11,16 @@ import './App.css';
 function App() {
   return (
     <>
-    <HashRouter><Menu/></HashRouter>
+   
     <HashRouter>
-      
+      <Menu/>
       <Switch>
         <Route exact path="/" render={ () => (<Redirect to="/employees" />) }   />
         <Route exact path="/employees" component={ListEmployees} />
-        <Route exact path="/employees/:id" component={Menu} />
+        <Route exact path="/employees/:id" component={ListEmployees} />
         <Route exact path="/prizes" component={Menu} />
         <Route exact path="/prizes/:id" component={Menu} />
-        <Route exact path="/achievements" component={Menu} />
+        <Route exact path="/achievements" component={ListAchievement} />
        {/*en caso de que no se encuentre la ruta*/}
         <Route component={NotFound} />
       </Switch>
